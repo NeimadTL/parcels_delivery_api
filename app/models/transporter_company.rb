16b8f37968f20b3eb29129cb4a925549c975b655
neimad971 <damientalbot26@gmail.com>
2019-10-11 16:08:17 +0200

    adds uniqueness validation on transporter company siret along with test

--- app/models/transporter_company.rb
+++ app/models/transporter_company.rb
@@ -1,6 +1,6 @@
 class TransporterCompany < ApplicationRecord
   validates :name, presence: true
-  validates :siret, presence: true
+  validates :siret, presence: true, uniqueness: true
 
   has_many :transporters_postcodes_relationships
   has_many :post_codes, through: :transporters_postcodes_relationships
--- spec/models/transporter_company_spec.rb
+++ spec/models/transporter_company_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe TransporterCompany, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :siret }
+  it { should validate_uniqueness_of :siret }
 
   it { should have_many(:transporters_postcodes_relationships) }
   it { should have_many(:post_codes).through(:transporters_postcodes_relationships) }

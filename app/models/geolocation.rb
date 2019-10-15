741501a741d6313e0af25c3b35bfce4ba8317558
neimad971 <damientalbot26@gmail.com>
2019-10-15 22:02:30 +0200

    add validations on geolocation model and tests along with them

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,3 +1,9 @@
 class Geolocation < ApplicationRecord
+
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :altitude, presence: true
+
   belongs_to :carrier
+
 end
--- spec/models/geolocation_spec.rb
+++ spec/models/geolocation_spec.rb
@@ -3,5 +3,8 @@ require 'rails_helper'
 RSpec.describe Geolocation, type: :model do
 
   it { should belong_to(:carrier) }
+  it { should validate_presence_of :latitude }
+  it { should validate_presence_of :longitude }
+  it { should validate_presence_of :altitude }
   
 end
